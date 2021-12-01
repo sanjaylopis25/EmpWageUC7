13fa8696da15cab3297fb578facf08318f267490
sanajylopis25 <sanajylopis25@gmail.com>
2021-12-01 18:40:36 +0530

    First Commit

---
+++ EmpWageUC7/EmpWageUC7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpWageUC7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RefactorUC7.computeEmpWage();
+        }
+    }
+}
